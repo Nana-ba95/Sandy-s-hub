a08e45b92125a57fb0fa9b1dcc98f2dcd480eec4
Nana-ba95 <sandyadjeibaduwonda95@gmail.com>
2026-07-23 12:58:18 +0400

    Initialize main.jsx with React application setup

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import BusinessApp from "./BusinessApp.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BusinessApp />
+  </React.StrictMode>
+);
