74be73c974e9c9a1c011fd099438c018710cab81
Nana-ba95 <sandyadjeibaduwonda95@gmail.com>
2026-07-23 12:54:49 +0400

    Delete arc/main.jsx

--- arc/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import BusinessApp from "./BusinessApp.jsx";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BusinessApp />
-  </React.StrictMode>
-);
