78e63754d1ae4a4956a54d8d04805fbbf7d65267
Nana-ba95 <sandyadjeibaduwonda95@gmail.com>
2026-07-24 02:37:18 +0400

    Add ErrorCatcher component to handle errors

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,30 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import BusinessApp from "./BusinessApp.jsx";
 
+class ErrorCatcher extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { error: null };
+  }
+  static getDerivedStateFromError(error) {
+    return { error };
+  }
+  render() {
+    if (this.state.error) {
+      return (
+        <pre style={{ padding: 20, color: "red", whiteSpace: "pre-wrap", fontFamily: "monospace" }}>
+          {String(this.state.error.stack || this.state.error)}
+        </pre>
+      );
+    }
+    return this.props.children;
+  }
+}
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BusinessApp />
+    <ErrorCatcher>
+      <BusinessApp />
+    </ErrorCatcher>
   </React.StrictMode>
 );
